4e0281fad55503faef0e7467630661ca4c822f94
shandanaktk <sammy39998@gmail.com>
2024-10-08 02:46:18 +0500

    Final

---
+++ weather/src/api.jsx
@@ -0,0 +1,11 @@
+export const geoAPIOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "d5b1713f47msha3acbc8b95d095fp1dd006jsnc2ba5a88c6fc",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "66cb0f17b591cd63e3adca91b20d9fb8";
